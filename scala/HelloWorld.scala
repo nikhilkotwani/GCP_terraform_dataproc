e601758571ea3bf81b8d36598f26d9814d3629ff
Nikhil Kotwani <nikhilkotwani@192-168-1-107.tpgi.com.au>
2020-05-07 14:22:00 +1000

    Changes1

--- scala/HelloWorld.scala
+++ scala/HelloWorld.scala
@@ -1,6 +1,6 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
-    println("Hello , this is a second change")
+    println("Hello , this is a change")
   }
 }
